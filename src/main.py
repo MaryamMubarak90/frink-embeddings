3f1cf2ffd363f268505d2d8b2375a65572a7ec74
Lisa Stillwell <lisa@renci.org>
2025-08-14 11:08:29 -0400

    #1 updated to use new config file layout

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import yaml
 from rdflib import URIRef
 from rdflib_hdt import HDTDocument
 from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient, models
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -18,6 +19,17 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
+
+def getIriKeyFromValue(iri_types, iri_type_value):
+
+    for iris in iri_types:
+        for key, value in iris.items():
+            if isinstance(value, list) and iri_type_value in value:
+                return key
+
+    return None
+
+
 # get list of iri types from config file
 def getIriTypes(conf_file):
     iri_types = []
@@ -25,7 +37,7 @@ def getIriTypes(conf_file):
     try:
         with open(conf_file, 'r') as conf:
             conf_yaml = yaml.safe_load(conf)
-            iri_types = conf_yaml['iriTypes']
+            iri_types = conf_yaml['irisToEmbed']
 
         conf.close()
 
@@ -34,17 +46,63 @@ def getIriTypes(conf_file):
 
     return iri_types
 
-# function to determine whether this IRI should
-# be included in list of triple to be embedded
-# this function consults a config file
-# to determine which IRI types to include
-def needsEmbedding(iri_types, predicate):
-    ret = False
+def getAllIriTypeValues(iri_types):
+    iri_values = []
 
-    if str(predicate) in iri_types:
-        ret = True
+    for type in iri_types:
+        value_list_obj = type.values()
+        value_list = list(value_list_obj)[0]
+        iri_values += value_list
 
-    return ret
+    return iri_values
+
+
+# this function takes a list of dicts and creates
+# another dict that take the format of:
+# <triple subject>: <string - sentence to embed>
+# the string to embed looks something like this:
+# 'HELLO; also known as  ERS-1_BYU_L3_OW_SIGMA0_ENHANCED; description: WHEEEEE!; subject: HELLO; subject: ERS-1 Gridded Level 3 Enhanced Resolution Sigma-0 from BYU'
+# note the labels are a special case and not preceded by a <key>: before the value
+def createSentences(embed_list):
+
+    sentence_dict = {}
+    sentences_to_embed_dict = {}
+
+    for item in embed_list:
+        subject = item.get('subject')
+        obj = item.get('object')
+        key = item.get('config_key')
+        if subject is not None and key is not None:
+            if subject not in sentence_dict:
+                sentence_dict[subject] = []
+            sentence_dict[subject].append({key: obj})
+
+    # now create sentence to embed for each subject (s)
+    also_str = "also known as"
+    for key, value in sentence_dict.items():
+        sentence = None
+
+        # special case - don't add the key for labels
+        # use following for testing
+        # val = [{'label': 'HELLO'},{'description': 'WHEEEEE!'},{'subject': 'HELLO'},{'subject': 'ERS-1 Gridded Level 3 Enhanced Resolution Sigma-0 from BYU'},{'label': 'ERS-1_BYU_L3_OW_SIGMA0_ENHANCED'}]
+        labels = [d["label"] for d in value if "label" in d]
+        if labels is not None:
+            sentence = labels[0]
+            if len(labels) > 1:
+                sentence += f"; {also_str} "
+                for label in labels[1:]:
+                    sentence += f" {label}"
+
+        # now append the rest of the predicates
+        for value_dict in value:
+            for k, v in value_dict.items():
+                if k != "label":
+                    sentence += f"; {k}: {v}"
+
+        sentences_to_embed_dict[key] = sentence
+
+    return sentences_to_embed_dict
+
 
 # create embeddings from rdf triples
 def main(input_file: pathlib.Path, config_file: pathlib.Path):
@@ -54,43 +112,24 @@ def main(input_file: pathlib.Path, config_file: pathlib.Path):
     logger.info(f"subjects: {doc.nb_subjects}  predicates: {doc.nb_predicates}  objects: {doc.nb_objects}")
 
     iri_types = getIriTypes(config_file)
+    iri_values = getAllIriTypeValues(iri_types)
 
-    subject = None
-    object_list = []
-    triples_dict = {}
-    triples_iterator, cardinality = doc.search((None, None, None))
-    # loop through triples that are IRIs
-    # since triple subjects seem to be in order
-    # (ignore relationships for now?)
-    # also, only include IRI predicates that are
-    # listed in the config file
+    sentences_to_embed_dict = {}
+    embed_list = []
+    triples, cardinality = doc.search((None, None, None))
+
     try:
-        for s, p, o in triples_iterator:
-            if isinstance(p, URIRef):
+        # first collect all of the triples desired - as defined
+        # by the predicates listed in the config file
+        for s, p, o in triples:
+            if str(p) in iri_values and isinstance(p, URIRef):
+                key = getIriKeyFromValue(iri_types, str(p))
+                embed_dict = {"config_key": key, "subject": str(s), "predicate": str(p), "object": str(o)}
+                embed_list.append(embed_dict)
 
-                if subject is None: # first time through
-                    subject = str(s)
-                    # check config file here to make sure we
-                    # want to include this predicate in the embedding
-                    if needsEmbedding(iri_types, p):
-                        object_list.append(f"{str(p)} {str(o)}")
+        # now create a list of sentences to embed
+        sentences_to_embed_dict = createSentences(embed_list)
 
-                elif str(s) != subject: # we have changed to a new subject
-                    # save subject and object list from last interation
-                    if len(object_list) > 0:
-                        triples_dict[subject] = ";".join(object_list)
-                        # reset object list
-                        object_list = []
-                    # new subject
-                    subject = str(s)
-                    if needsEmbedding(iri_types, p):
-                        object_list.append(f"{str(p)} {str(o)}")
-
-                else: # still on same subject, collecting predicate/object pairs
-                    # check config file here to make sure we
-                    # want to include this predicate in the embedding
-                    if needsEmbedding(iri_types, p):
-                        object_list.append(f"{str(p)} {str(o)}")
 
     except Exception as e:
         logger.error(f"An error occurred while parsing file {input_file}: {e}")
@@ -99,18 +138,56 @@ def main(input_file: pathlib.Path, config_file: pathlib.Path):
         model = SentenceTransformer('all-MiniLM-L6-v2')
         logger.debug("loaded model")
 
-        # create embeddings
-        logger.debug("about to encode embeddings")
+        # create embeddings and write to a csv file
+        # logger.debug("about to encode embeddings")
         # create an embedding for each unique subject
         # and write to the csv file
-        csv_file = os.path.splitext(os.path.basename(input_file))[0] + ".csv"
-        with open(csv_file, "w", newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['iri', 'embedding'])
-            for k, v in triples_dict.items():
+        # csv_file = os.path.splitext(os.path.basename(input_file))[0] + ".csv"
+        # with open(csv_file, "w", newline='') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow(['iri', 'embedding'])
+        #     for k, v in sentences_to_embed_dict.items():
+        #         embedding = model.encode(v)
+        #         writer.writerow([k, embedding])
+        # f.close()
+
+        logger.debug("about to encode embeddings")
+
+        # create an embedding for each unique subject
+        # and write to a qdrant collection
+        # now connect to Qdrant client
+        client = QdrantClient(url="http://localhost:6333", timeout=30)
+        # create a collection name
+        collection_name = os.path.splitext(os.path.basename(input_file))[0]
+        if client.collection_exists(collection_name):
+            # collection already exists so notify and exit
+            logger.info(f"Collection '{collection_name}' already exists.")
+            exit(0)
+        else:
+            # collection does not exist - continue to create and populate
+            logger.debug(f"Collection '{collection_name}' does not exist.")
+            client.create_collection(collection_name=collection_name,
+                                     vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE))
+            idx = 0
+            points = []
+            print(len(sentences_to_embed_dict.items()))
+            for k, v in sentences_to_embed_dict.items():
+                idx+=1
                 embedding = model.encode(v)
-                writer.writerow([k, embedding])
-        f.close()
+                points.append(
+                    models.PointStruct(
+                        id=idx,  # Unique ID for each point
+                        vector=embedding,
+                        payload={"iri": k}  # Add metadata (payload)
+                    )
+                )
+                # batch upserts to avoid timeouts
+                if idx % 1000 == 0:
+                    client.upsert(collection_name=collection_name, points=points)
+                    points = []
+
+            # final upsert
+            client.upsert(collection_name=collection_name, points=points)
 
     except Exception as e:
         logger.error(f"An error occurred while embedding triples {input_file}: {e}")
